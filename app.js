fe9437d3b6cc8afbb025a87f32804bcf8e382960
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-06-15 10:22:49 +0200

    adibide simplea funtzionatzen

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // A $( document ).ready() block.
 $(document).ready(function () {
-    
+    $('.item').draggable({revert: false});
+
+    $('#caja').droppable({
+        drop: function( event, ui ) {
+          $( this )
+            .css( "opacity", "1" );
+            //carritoan 1 gehitu
+            
+        }
+      });
 
 
 });
